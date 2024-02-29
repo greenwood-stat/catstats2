45a087576d03d5df9be485eac82eaa11c2698046
greenwood-stat <greenwood.stat@gmail.com>
2024-02-29 13:43:00 -0700

    Simplified plotly call to finally fix issue

--- R/ggdredgeplotly.R
+++ R/ggdredgeplotly.R
@@ -79,7 +79,7 @@ ggdredgeplotly = function(data,
       theme_bw()
 
 
-    gplotly <- style(ggplotly(gplot2, tooltip = c("x", "LabelInfo")), hoverinfo = "skip", traces = 1)
+    gplotly <- ggplotly(gplot2, tooltip = c("x", "LabelInfo"))
     return(gplotly)
     }
 
